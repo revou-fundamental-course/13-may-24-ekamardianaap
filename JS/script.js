bcbb78448a1b2119f1b7811d177a73d9415e7221
Eka Mardiana Putri Rahmawati <ekamardianaa21@gmail.com>
2024-05-17 23:21:57 +0700

    update file

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,13 @@
-// Ini JS
+function calculateArea(event) {
+    event.preventDefault();
+    const alas = parseFloat(document.getElementById('alas').value);
+    const tinggi = parseFloat(document.getElementById('tinggi').value);
+    const area = 0.5 * alas * tinggi;
+    
+    const resultElement = document.getElementById('result');
+    resultElement.innerHTML = `
+        L = 1/2 x a x t <br>
+        L = 1/2 x ${alas} x ${tinggi} <br>
+        L = ${area}
+    `;
+}
